8d00960d22e31d0d046a6d04565de0062f55561a
alifarzanehrad <alifarzane.rad@gmail.com>
2026-07-25 23:53:43 +0330

    delete mux 32 bit and 5 bit

--- mux32bit/mux32bit.v
+++
@@ -1,9 +0,0 @@
-`timescale 1ns/1ps
-module mux32bit (
-    input            sel,
-    input      [31:0]in0,
-    input      [31:0]in1,
-    output     [31:0]out
-);
-    assign out = (sel == 1'b0)? in0 : in1;
-endmodule
